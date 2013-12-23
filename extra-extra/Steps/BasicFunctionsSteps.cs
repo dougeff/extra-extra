eed1c208b073eb9b147ddcae395461dc1f4a6400
dougeff <djfittipaldi@gmail.com>
2013-12-23 17:41:22 -0500

    moving app to WPF

---
+++ extra-extra/Steps/BasicFunctionsSteps.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using TestStack.White;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.WindowItems;
+
+namespace extra_extra.Steps
+{
+    [Binding]
+    public class BasicFunctionsSteps
+    {
+        private static Application _applicationToTest;
+        private static Window _startingWindow;
+
+        [BeforeFeature]
+        public static void LaunchApp()
+        {
+            _applicationToTest = Application.Launch(@"..\..\..\extra-extra\bin\Debug\extra-extra.exe");
+            _startingWindow = _applicationToTest.GetWindow("Extra Extra");
+        }
+
+
+        [Given(@"I have entered text into the query field")]
+        public void GivenIHaveEnteredTextIntoTheQueryField()
+        {
+            var textQuery = _startingWindow.Get<TextBox>("TextQuery");
+            textQuery.Enter("blah");
+        }
+
+        [When(@"I press search")]
+        public void WhenIPressSearch()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"it should fetch query results")]
+        public void ThenItShouldFetchQueryResults()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"they should be minimized with a button to expand")]
+        public void ThenTheyShouldBeMinimizedWithAButtonToExpand()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
